ff83382940abe1f5218ff9d2449b9b66648045c7
BlueblueX <94584792+BBlueSkyy@users.noreply.github.com>
2026-09-18 22:03:46 -0300

    spirv: add storage CAS helper state

--- backend/spirv/spirv_emit_context.h
+++ backend/spirv/spirv_emit_context.h
@@ -287,6 +287,8 @@ public:
     Id f32x2_min_cas{};
     Id f32x2_max_cas{};
 
+    Id write_storage_cas_loop_func{};
+
     Id load_global_func_u32{};
     Id load_global_func_u32x2{};
     Id load_global_func_u32x4{};
@@ -334,6 +336,7 @@ private:
     void DefineTextures(const Info& info, u32& binding, u32& scaling_index);
     void DefineImages(const Info& info, u32& binding, u32& scaling_index);
     void DefineAttributeMemAccess(const Info& info);
+    void DefineWriteStorageCasLoopFunction(const Info& info);
     void DefineGlobalMemoryFunctions(const Info& info);
     void DefineRescalingInput(const Info& info);
     void DefineRescalingInputPushConstant();
